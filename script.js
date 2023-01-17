d0b0d1022e26e27ec3ab073d36a0b128426dfb91
santoshsd98 <dikolesd98@gmail.com>
2023-01-17 15:29:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let firstname = "Santosh";
+let lastname = "Dikole";
+let age = 25;
+let martial_status = "yes";
+let country ="India";
+console.log("Your fullName is",firstname + lastname);
+console.log("yours age is",age);
+console.log("Are you  marrid ?", martial_status);
+console.log("which is yours country",country);
